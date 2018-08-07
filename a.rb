45a578c9247dc0b6ff02d5077f0776503e04dae3
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-08-07 21:51:51 +0530

    Create a.rb

---
+++ a.rb
@@ -0,0 +1,15 @@
+puts "This is main Ruby Program"
+
+END {
+   puts "Terminating Ruby Program"
+}
+BEGIN {
+   puts "Initializing Ruby Program"
+}
+#comments are not meant to be ignored .read me .
+
+= begin 
+block 
+mai comments 
+kar rahi hu :P 
+= end 
